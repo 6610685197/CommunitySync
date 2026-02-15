cc811ca54b0c207e2b08cd0673f6e3a5eac7c7ed
Kittichet Sintaosathapon <56689076+Lnynano@users.noreply.github.com>
2026-02-15 20:24:49 +0700

    add discord auth

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
+    path("auth/discord/", views.discord_auth, name="auth_discord"),
     # Destination URLs
     path("dashboard/", views.admin_dashboard, name="admin_dashboard"),
     path("home/", views.resident_home, name="resident_home"),

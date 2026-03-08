5a537cc99668a167d69300473749a7077bd3bd33
polthagorn <6610685262@cn330>
2026-03-09 00:08:00 +0700

    make create user

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -5,8 +5,10 @@ urlpatterns = [
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
     path("auth/discord/", views.discord_auth, name="auth_discord"),
+    path("create-account/", views.create_account, name="create_account"),
     # Destination URLs
     path("dashboard/", views.admin_dashboard, name="admin_dashboard"),
     path("home/", views.resident_home, name="resident_home"),
     path("", views.index, name="index"),
+    path("users/", views.user_list, name="user_list"),
 ]
